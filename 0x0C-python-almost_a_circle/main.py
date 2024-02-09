811ca2f1544a1da73aa870a42788ed08b1232ede
Mustapha KHALID <mosstaf096@gmail.com>
2024-02-08 22:48:21 -0800

    done

---
+++ 0x0C-python-almost_a_circle/main.py
@@ -0,0 +1,16 @@
+from models.rectangle import Rectangle
+
+r1 = Rectangle(10, 10, 10, 10)
+print(r1)
+
+r1.update(height=1)
+print(r1)
+
+r1.update(width=1, x=2)
+print(r1)
+
+r1.update(y=1, width=2, x=3, id=89)
+print(r1)
+
+r1.update(x=1, height=2, y=3, width=4)
+print(r1)
